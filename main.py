da355a02aca5263cd35360c3618916af069a37e6
jaxonyue <jaxonyue>
2023-11-12 05:49:46 +0000

    a

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-from mylib.extract import extract
-from mylib.transform_load import load
-#from mylib.query_viz import query_transform, viz
-import os
+"""
+Main cli or app entry point
+"""
+
+def main():
+    print(1)
+    return 0
 
 
 if __name__ == "__main__":
-    current_directory = os.getcwd()
-    print(current_directory)
-    extract()
-    #load()
-    #query_transform()
-    #viz()
+    main()
--- test_main.py
+++ test_main.py
@@ -1,9 +1,9 @@
+"""
+Test goes here
+
+"""
+
 from main import main
 
-
 def test_main():
-    assert main() is None
-
-
-if __name__ == "__main__":
-    test_main()
+    assert main() == 0
