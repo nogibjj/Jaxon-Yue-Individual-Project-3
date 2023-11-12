547e1319e022ccf249ddbe14c03171230bcbe292
jaxonyue <jaxonyue>
2023-11-12 05:09:57 +0000

    a

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from mylib.loadData import query
-
-
-def main():
-    query()
+from mylib.extract import extract
+from mylib.transform_load import load
+#from mylib.query_viz import query_transform, viz
+import os
 
 
 if __name__ == "__main__":
-    main()
+    current_directory = os.getcwd()
+    print(current_directory)
+    extract()
+    #load()
+    #query_transform()
+    #viz()
